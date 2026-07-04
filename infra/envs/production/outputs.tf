fcb9db570ec227f54c1dc39cd2787739c80832e1
Keiter0309 <vuongtuankiet33@gmail.com>
2026-07-04 11:41:13 +0700

    fix(infra): use existing CloudFront media domain

--- infra/envs/production/outputs.tf
+++ infra/envs/production/outputs.tf
@@ -22,10 +22,6 @@ output "ecs_cluster_name" {
   value = aws_ecs_cluster.main.name
 }
 
-output "cloudfront_domain_name" {
-  value = aws_cloudfront_distribution.media.domain_name
-}
-
 output "media_domain_name" {
   value = var.media_domain_name
 }
