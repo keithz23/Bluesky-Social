f9825f0e507dee35f1ec937b750ce3d41f9bc697
Keiter0309 <vuongtuankiet33@gmail.com>
2026-07-22 14:50:55 +0700

    feat(admin): add roles and permissions modules

--- social-be/src/modules/admin/roles/roles.controller.ts
+++ social-be/src/modules/admin/roles/roles.controller.ts
@@ -6,38 +6,65 @@ import {
   Patch,
   Param,
   Delete,
+  Query,
 } from '@nestjs/common';
 import { RolesService } from './roles.service';
 import { CreateRoleDto } from './dto/create-role.dto';
 import { UpdateRoleDto } from './dto/update-role.dto';
-import { CurrentUser } from 'src/common/decorators/current-user.decorator';
+import { RoleQueryDto } from './dto/role-query,dto';
+import { DeleteRoleDto } from './dto/delete-role.dto';
+import { assignPermissionsDto } from './dto/assign-permissions.dto';
 
 @Controller('roles')
 export class RolesController {
   constructor(private readonly rolesService: RolesService) {}
 
-  @Post('create-role')
+  @Post()
   create(@Body() createRoleDto: CreateRoleDto) {
     return this.rolesService.create(createRoleDto);
   }
 
   @Get()
-  findAll() {
-    return this.rolesService.findAll();
+  findAll(@Query() query: RoleQueryDto) {
+    return this.rolesService.findAll(query);
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.rolesService.findOne(+id);
+  @Get(':roleId')
+  findOne(@Param('roleId') roleId: string) {
+    return this.rolesService.findOne(roleId);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateRoleDto: UpdateRoleDto) {
-    return this.rolesService.update(+id, updateRoleDto);
+  @Patch(':roleId')
+  update(
+    @Param('roleId') roleId: string,
+    @Body() updateRoleDto: UpdateRoleDto,
+  ) {
+    return this.rolesService.update(roleId, updateRoleDto);
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.rolesService.remove(+id);
+  @Delete()
+  delete(@Body() deleteRoleDto: DeleteRoleDto) {
+    return this.rolesService.delete(deleteRoleDto);
+  }
+
+  // Assign permissions
+  @Post(':roleId/permissions')
+  assignPermissions(
+    @Param('roleId') roleId: string,
+    @Body() assignPermissionsDto: assignPermissionsDto,
+  ) {
+    return this.rolesService.assignPermissions(
+      roleId,
+      assignPermissionsDto.permissionIds,
+    );
+  }
+
+  // Revoke permission
+  @Delete(':roleId/permissions/:permissionId')
+  revokePermission(
+    @Param('roleId') roleId: string,
+    @Param('permissionId') permissionId: string,
+  ) {
+    return this.rolesService.revokePermission(roleId, permissionId);
   }
 }
